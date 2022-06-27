286207a4b5ef721ead132ee7444b2c091fd2a2d2
sadegh-dev <sadegh.rez@gmail.com>
2022-06-27 12:57:47 +0430

    #project5 -develop sender.py

---
+++ project5/sender.py
@@ -0,0 +1,18 @@
+import pika
+
+conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+ch  = conn.channel()
+
+ch.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+messages = {
+	'info' : 'info about line ' ,
+	'error' : 'error about line ' ,
+	'warning' : 'warning about line '
+}
+
+for tt, mm in messages.items():
+	ch.basic_publish(exchange='direct_logs', routing_key=tt , body = mm)
+	print(f'sent message {tt} !')
+conn.close()
+
