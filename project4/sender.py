6a98305550c645a2ecf6e84fca2a556173d45a6d
sadegh-dev <sadegh.rez@gmail.com>
2022-06-27 10:51:09 +0430

    #project4 -create files -develop sender.py

---
+++ project4/sender.py
@@ -0,0 +1,14 @@
+import pika
+
+conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+ch = conn.channel()
+
+ch.exchange_declare(exchange='fan_logs', exchange_type='fanout')
+
+the_message = 'this is Testing fanout exchange !'
+
+ch.basic_consume(exchange='fan_logs', routing_key='', body=the_message )
+
+print('Sent message !')
+
+conn.close()
