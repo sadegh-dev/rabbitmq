054e4b3d5bf82690c6337f98fb36256d92abcddd
sadegh-dev <sadegh.rez@gmail.com>
2022-06-27 11:41:32 +0430

    #project4 -develop complete project4

--- project4/sender.py
+++ project4/sender.py
@@ -7,7 +7,7 @@ ch.exchange_declare(exchange='fan_logs', exchange_type='fanout')
 
 the_message = 'this is Testing fanout exchange !'
 
-ch.basic_consume(exchange='fan_logs', routing_key='', body=the_message )
+ch.basic_publish(exchange='fan_logs', routing_key='', body=the_message )
 
 print('Sent message !')
 
