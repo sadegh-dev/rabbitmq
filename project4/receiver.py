5b0d6da2a3e2537dc28459111bbad2e9afe37f00
sadegh-dev <sadegh.rez@gmail.com>
2022-06-28 17:50:59 +0430

    #project4 -develop complete

--- project4/receiver.py
+++ project4/receiver.py
@@ -1,20 +1,42 @@
 import pika
 
-conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+conn = pika.BlockingConnection(pika.ConnectionParameters(
+    host='localhost'
+    )
+)
+
 ch = conn.channel()
 
-ch.exchange_declare(exchange='fan_logs', exchange_type='fanout')
+ch.exchange_declare(
+    exchange = 'fan_logs', 
+    exchange_type = 'fanout'
+)
 
-result = ch.queue_declare(queue='', exclusive=True)
+result = ch.queue_declare(
+    queue = '' , 
+    exclusive = True
+)
 
 qname = result.method.queue
-ch.queue_bind(exchange='fan_logs', queue=qname)
+
+ch.queue_bind(
+    exchange = 'fan_logs', 
+    queue = qname
+)
 
 print('Wating ...!')
 
 def callback(ch, method, properties, body):
     print(f'received {body}')
     print(f'qname is {qname}')
+    print('--------------------')
 
-ch.basic_consume(queue=qname, on_message_callback=callback, auto_ack=True)
+
+ch.basic_consume(
+    queue = qname , 
+    on_message_callback = callback , 
+    auto_ack = True
+)
+
 ch.start_consuming()
+
