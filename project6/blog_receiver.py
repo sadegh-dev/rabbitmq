d6dcc5b5e84b6c8bb851f1a72900a3c36f862e4c
sadegh-dev <sadegh.rez@gmail.com>
2022-06-28 18:36:24 +0430

    #project6 -clean code

--- project6/blog_receiver.py
+++ project6/blog_receiver.py
@@ -1,18 +1,34 @@
 import pika
 
-conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+conn = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='localhost'
+    )
+)
+
 ch = conn.channel()
 
-ch.exchange_declare(exchange='topic_logs', exchange_type='topic')
+ch.exchange_declare(
+    exchange='topic_logs', 
+    exchange_type='topic'
+)
 
-result = ch.queue_declare(queue='', exclusive=True)
+result = ch.queue_declare(
+    queue='', 
+    exclusive=True
+)
 
 qname = result.method.queue
 
 # binding key ##########
 binding_key = '*.weblog.*'
 
-ch.queue_bind(exchange='topic_logs', queue=qname, routing_key=binding_key )
+ch.queue_bind(
+    exchange='topic_logs', 
+    queue=qname, 
+    routing_key=binding_key
+)
+
 
 print('Wating for logs. To exit press CTRL+C')
 
@@ -22,9 +38,10 @@ def callback(ch, method, properties, body):
     print(f'details : {method.routing_key}')
     print('******************')
 
-ch.basic_consume(queue=qname, on_message_callback=callback, auto_ack= True)
+ch.basic_consume(
+    queue=qname, 
+    on_message_callback=callback, 
+    auto_ack= True
+)
 
 ch.start_consuming()
-
-
-
--- project6/error_receiver.py
+++ project6/error_receiver.py
@@ -1,18 +1,33 @@
 import pika
 
-conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+conn = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='localhost'
+    )
+)
+
 ch = conn.channel()
 
-ch.exchange_declare(exchange='topic_logs', exchange_type='topic')
+ch.exchange_declare(
+    exchange='topic_logs', 
+    exchange_type='topic'
+)
 
-result = ch.queue_declare(queue='', exclusive=True)
+result = ch.queue_declare(
+    queue='', 
+    exclusive=True
+)
 
 qname = result.method.queue
 
 # binding key ##########
 binding_key = 'error.#'
 
-ch.queue_bind(exchange='topic_logs', queue=qname, routing_key=binding_key )
+ch.queue_bind(
+    exchange='topic_logs', 
+    queue=qname, 
+    routing_key=binding_key
+)
 
 print('Wating for logs. To exit press CTRL+C')
 
@@ -24,9 +39,11 @@ def callback(ch, method, properties, body):
     print(f'details : {method.routing_key}')
     print('******************')
 
-ch.basic_consume(queue=qname, on_message_callback=callback, auto_ack= True)
+
+ch.basic_consume(
+    queue=qname, 
+    on_message_callback=callback, 
+    auto_ack= True
+)
 
 ch.start_consuming()
-
-
-
