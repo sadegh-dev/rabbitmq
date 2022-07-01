79144adfa8238a455a87e0bdd23bb78facfcdcac
sadegh-dev <sadegh.rez@gmail.com>
2022-07-01 10:36:22 +0430

    #project7 -develop Client file

--- project7/sender.py
+++ project7/sender.py
@@ -22,14 +22,33 @@ class Sender:
             auto_ack = True
         )
 
-        def my_response(self, ch, method, proper, body):
-            if self.corr_id == proper.correlation_id :
-                self.response = body
+
+    def my_response(self, ch, method, proper, body):
+        if self.corr_id == proper.correlation_id :
+            self.response = body
 
 
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.ch.basic_publish(
+            exchange='' ,
+            routing_key = 'rpc_queue' ,
+            body = str(n) ,
+            properties = pika.BasicProperties(
+                reply_to = self.qname ,
+                correlation_id = self.coor_id ,
+            )
+        )
+        while self.response is None :
+            self.conn.process_data_events()
 
+        return self.response
 
 
 
 send = Sender()
 
+response = send.call(22)
+
+print(response)
